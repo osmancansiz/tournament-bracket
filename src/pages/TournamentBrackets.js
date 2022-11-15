4c8a0ce60f5572b64992f7cb8c8652cae9e0f3fd
Mustafa Osman Tekin <mustafa.tekin@demarkelabs.com>
2022-11-15 20:59:14 +0300

    handle quarter final teams

--- src/pages/TournamentBrackets.js
+++ src/pages/TournamentBrackets.js
@@ -6,6 +6,8 @@ export default function TournamentBrackets() {
   const [teams] = useState(initialTeams);
   const [groups] = useState(initialGroups);
   const [selectedTeams, setSelectedTeams] = useState([]);
+  const [quarterFinal, setQuarterFinal] = useState([]);
+  const [semiFinal, setSemiFinal] = useState([]);
 
   const handleClick = (team) => {
     if (selectedTeams.findIndex((i) => i.id === team.id) !== -1) return;
@@ -18,6 +20,20 @@ export default function TournamentBrackets() {
       team.rank = groupMember[0].rank + 1;
       setSelectedTeams([...selectedTeams, team]);
     }
+  };
+
+  const setQuarterFinalTeams = (team, side) => {
+    if (quarterFinal.findIndex((i) => i.id === team.id) !== -1) return;
+
+    const filteredTeams = quarterFinal.filter((i) => i.side === side);
+    if (filteredTeams.length === 0) {
+      team.side = side;
+      setQuarterFinal([...quarterFinal, team]);
+    }
+  };
+
+  const setSemiFinalTeams = (team, side) => {
+    console.log(team, side);
   };
 
   return (
@@ -53,110 +69,199 @@ export default function TournamentBrackets() {
               );
             })}
           </div>
-          <div className="col-span-1">
-            <div className="flex flex-col justify-around gap-10 h-full relative text-center">
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "A" && i.rank === 1)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
+          <div className="col-span-1 space-y-16">
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "A" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 1)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "B" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 1)}>
+                    {team.name}
+                  </div>
+                ))}
+            </div>
 
-                {selectedTeams
-                  .filter((i) => i.group === "B" && i.rank === 2)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-              </div>
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "C" && i.rank === 1)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "D" && i.rank === 2)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-              </div>
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "E" && i.rank === 1)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "F" && i.rank === 2)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-              </div>
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "G" && i.rank === 1)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "H" && i.rank === 2)
-                  .map((team, index) => (
-                    <div key={index} className="my-2">{team.name}</div>
-                  ))}
-              </div>
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "C" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 2)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "D" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 2)}>
+                    {team.name}
+                  </div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "E" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 3)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "F" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 3)}>
+                    {team.name}
+                  </div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "G" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 4)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "H" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 4)}>
+                    {team.name}
+                  </div>
+                ))}
             </div>
           </div>
-          <div className="col-span-6"></div>
+          <div className="col-span-1 space-y-16 ml-5">
+            <div className="bg-lime-500 w-32 h-20 mt-20">
+              {quarterFinal
+                .filter((i) => i.side === 1)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+              {quarterFinal
+                .filter((i) => i.side === 2)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20 mt-20">
+              {quarterFinal
+                .filter((i) => i.side === 3)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+              {quarterFinal
+                .filter((i) => i.side === 4)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+            </div>
+          </div>
+
+          <div className="col-span-4"></div>
+
+          <div className="col-span-1 space-y-16 mr-20">
+            <div className="bg-lime-500 w-32 h-20 mt-20">
+              {quarterFinal
+                .filter((i) => i.side === 5)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+              {quarterFinal
+                .filter((i) => i.side === 6)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20 mt-20">
+              {quarterFinal
+                .filter((i) => i.side === 7)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+              {quarterFinal
+                .filter((i) => i.side === 8)
+                .map((team) => (
+                  <div onClick={() => setSemiFinalTeams(team)}>{team.name}</div>
+                ))}
+            </div>
+          </div>
           <div className="col-span-1 space-y-16">
-            <div className="flex flex-col justify-around gap-10 h-full relative text-center">
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "B" && i.rank === 1)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "A" && i.rank === 2)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-              </div>
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "D" && i.rank === 1)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "C" && i.rank === 2)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-              </div>
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "F" && i.rank === 1)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "E" && i.rank === 2)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-              </div>
-              <div className="h-12 ">
-                {selectedTeams
-                  .filter((i) => i.group === "H" && i.rank === 1)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-                {selectedTeams
-                  .filter((i) => i.group === "G" && i.rank === 2)
-                  .map((team) => (
-                    <div className="my-2">{team.name}</div>
-                  ))}
-              </div>
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "B" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 5)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "A" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 5)}>
+                    {team.name}
+                  </div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "D" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 6)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "C" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 6)}>
+                    {team.name}
+                  </div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "F" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 7)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "E" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 7)}>
+                    {team.name}
+                  </div>
+                ))}
+            </div>
+
+            <div className="bg-lime-500 w-32 h-20">
+              {selectedTeams
+                .filter((i) => i.group === "H" && i.rank === 1)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 8)}>
+                    {team.name}
+                  </div>
+                ))}
+              {selectedTeams
+                .filter((i) => i.group === "G" && i.rank === 2)
+                .map((team) => (
+                  <div onClick={() => setQuarterFinalTeams(team, 8)}>
+                    {team.name}
+                  </div>
+                ))}
             </div>
           </div>
           <div className="col-span-2 space-y-10">
