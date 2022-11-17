6dbdf84b8e5ab27ecf39260e86d76bed699f0d16
Osman Cansız <ocansiz@st.medipol.edu.tr>
2022-11-18 00:54:32 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import TournamentBrackets from "./pages/TournamentBrackets";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <TournamentBrackets />;
+  return (
+    <Router basename="tournament-bracket">
+      <Routes>
+        <Route path="/" element={<TournamentBrackets />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
